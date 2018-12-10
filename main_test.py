d59c844074a92ce96c84e0f048bd3672e6a5704d
SabinaAlieva <sabina-alieva.taganrog@mail.ru>
2018-12-10 23:54:52 +0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,9 +1,9 @@
 from lxml import etree, objectify
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-FILENAME = "ropsha.xml"
+FILENAME = "taganrog.xml"
 
 with open(FILENAME) as f:
     xml = f.read()
@@ -24,7 +24,8 @@ for row in way:
         road = []
         
 node = root.node             
-nodes = {float(row.attrib['id']): [float(row.attrib['lat']), float(row.attrib['lon'])] for row in node}
+
+nodes = {int(row.attrib['id']): [float(row.attrib['lat']), float(row.attrib['lon'])] for row in node}
 
 count_node_in_roads = {int(row.attrib['id']): 0 for row in node}
 
@@ -45,8 +46,7 @@ for nd in nodes:
     
 for nd in nodes_to_delete:
     nodes.pop(nd)
-
-
+    
 list_adj = {nd:[] for nd in nodes}
 
 row = []
@@ -56,53 +56,30 @@ for road in roads:
         if nd in nodes:        
             row.append(nd)
     for i in range(len(row) - 1):   
-        list_adj[row[i]].append(float(row[i+1]))
-        list_adj[row[i+1]].append(float(row[i]))
+        list_adj[row[i]].append(int(row[i+1]))
+        list_adj[row[i+1]].append(int(row[i]))
     row = []
     
 for key in list_adj:
     list_adj[key] = list(set(list_adj[key]))
     
 line_csv = []
-"""with open("list_sochi.csv", "w") as csv_file:
+with open("list_taganrog.csv", "w") as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
     for key in list_adj:
         line_csv.append(key)
         for r in list_adj[key]:
             line_csv.append(r)
         writer.writerow(line_csv)
-        line_csv = []"""
-        
-import csv
-
-line = []
-lline = np.zeros(len(list_adj) + 1)      
-
-"""with open("matr_sochi.csv", "w") as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    
-    line.append(0)
-    for key in list_adj:
-        line.append(key)
-    writer.writerow(line)
-    
-    for key in list_adj:
-        for nd in list_adj[key]:
-            lline[0] = key
-            lline[line.index(nd)] = 1
-        writer.writerow(lline)
-        lline = np.zeros(len(list_adj) + 1)
-        """
+        line_csv = []
         
 hospitals = {}
 
-for nd in node:    
+for nd in node:     
     tag = nd.find("tag")
     if not (tag is None) and tag.attrib['v'] == 'hospital':
         hospitals[int(nd.attrib['id'])] = [float(nd.attrib['lat']), float(nd.attrib['lon'])]
         
-print len(hospitals)
-
 import math
 
 target = []
@@ -115,28 +92,9 @@ for h in hospitals:
             min_w = w
             nd_mai = nd
     target.append(nd_mai)
-    
-def Input_lon():
-    print (' 38.84 and 38.96')
-    global lon
-    lon = input()
-    if lon < 38.84  or lon > 38.96:
-        print ('again')
-        Input_lon()
-        
-def Input_lat():
-    print (' 47.20 and 47.28')
-    global lat
-    lat = input()
-    if lat < 47.20 or lat > 47.28:
-        print ('again')
-        Input_lat()
-        
-#Input_lon()
-#Input_lat()
 
-lon = 29.847
-lat = 59.728
+lat = 47.261885 
+lon = 38.908703
 
 min_w = 1000000
 for nd in nodes:
@@ -145,21 +103,71 @@ for nd in nodes:
         min_w = w
         start = nd
         
-        
-#fig = plt.gcf()
-#fig.set_size_inches(8, 12)    
-Lon_Lat = []
+from heapq import heappush
+from heapq import heappop 
 
-# for key in list_adj:
-#     for nd in list_adj[key]:
-#         plt.plot([nodes[key][1],nodes[nd][1]], [nodes[key][0],nodes[nd][0]], 'blue')
-
-#for h in hospitals:
-    #plt.plot(hospitals.get(h)[1], hospitals.get(h)[0], 'ro')
-#plt.plot(nodes.get(start)[1], nodes.get(start)[0],  'bo')
-
-#plt.show()
+def Dejicstra(a, b):
     
+    len_sh_way = {nd: 1000000 for nd in nodes}  
+    len_sh_way.update({a: 0})
+    
+    shortest_way = {nd: [] for nd in nodes} 
+    shortest_way.update({a: [a]})
+    
+    current_node = a
+    
+    nd_to_visit = []                 
+    deleted = []                    
+    nd_to_visit.append(a)
+    
+    sort_len = []  
+    
+    while(1):
+        if current_node != b:   
+            
+            sort_len = [len_sh_way.get(nd) for nd in nd_to_visit]  
+            sort_len.sort()            
+                     
+            for key in nd_to_visit: 
+                if len_sh_way.get(key) == sort_len[0]: 
+                    current_node = key              
+                    deleted.append(key)            
+                    break
+            
+            nd_to_vis = list(tuple(list_adj.get(current_node))) 
+            
+            for nd in nd_to_vis:           
+                nd_to_visit.append(nd)
+            nd_to_visit = list(set(nd_to_visit)) 
+                
+            for nd in deleted:      
+                if nd_to_visit.count(nd) > 0:
+                    nd_to_visit.remove(nd)    
+            
+            for u in nd_to_visit:         
+                weight = math.sqrt((nodes.get(u)[1] + nodes.get(current_node)[1])**2 + (nodes.get(u)[0] + nodes.get(current_node)[0])**2)
+                
+                if len_sh_way.get(u) > len_sh_way.get(current_node) + weight:         
+                
+                    w = len_sh_way.get(current_node) + weight
+                    len_sh_way.update({u: w}) 
+                                   
+                    mas = list(tuple(shortest_way.get(current_node)))
+                    mas.append(u)
+                    m = list(tuple(mas))
+                    shortest_way.update({u: m}) 
+                    
+            if len(nd_to_visit) == 0:
+                return shortest_way.get(b)
+                break
+        
+        else: 
+            break
+        
+    return (shortest_way.get(b))
+
+
+
 from collections import deque
 
 def Levita(a, b):
@@ -169,8 +177,8 @@ def Levita(a, b):
     shortest_way = {nd: [] for nd in nodes} 
     shortest_way.update({a: [a]})
     
-    nodes_already_calc = [] 
-    common_queue = deque((a,))      
+    nodes_already_calc = []
+    common_queue = deque((a,))       
     rush_queue = deque()         
     
     nodes_not_calc = [nd for nd in nodes] 
@@ -222,41 +230,6 @@ def Levita(a, b):
         nodes_already_calc.append(cur_node)     
                           
     return(shortest_way.get(b))
-    
-
-
-def PlotResult(a, b, way):
-    #fig = plt.gcf()
-    #fig.set_size_inches(8, 12)   
-    Lon_Lat = []
-
-
-#     for key in list_adj:
-#         for nd in list_adj[key]:
-#             plt.plot([nodes[key][1],nodes[nd][1]], [nodes[key][0],nodes[nd][0]], 'blue')
-
-    #plt.plot(nodes.get(a)[1], nodes.get(a)[0], 'ro')
-    #plt.plot(nodes.get(b)[1], nodes.get(b)[0], 'ro')
-
-    
-    for nd in way:                 
-        Lon_Lat.append([nodes.get(nd)[1], nodes.get(nd)[0]])
-    Lon_Lat = np.array(Lon_Lat)   
-    #plt.plot(Lon_Lat[::, 0], Lon_Lat[::, 1], 'red')
-    Lon_Lat = []
-
-
-    #plt.show()
-    
-import time
-
-start_time = time.time()
-#way = Dejicstra(start, target[3])
-print("--- %s seconds ---" % (time.time() - start_time))
-
-start_time = time.time()
-way = Levita(start, target[3])
-print("--- %s seconds ---" % (time.time() - start_time))
 
 
 import networkx as nx
@@ -273,24 +246,6 @@ for nd in list_adj:
         
 nodesi = {str(key): [nodes.get(key)[1], nodes.get(key)[0]] for key in nodes}
 
-#def DrawGraph():
-    #nx.draw_networkx(GG, pos=nodesi, node_size = 0.1, width = 0.2, with_labels = False)
-    #plt.axis('on')
-    #plt.show()
-    
-
-#def DrawSaveShortestWay(GG, way, start, end):
-    #nx.draw_networkx(GG, pos=nodesi, node_size = 0, width = 0.2, with_labels = False, node_color='black', edge_color='black')
-    #h = GG.subgraph(way)
-    #nx.draw_networkx_nodes(h,pos=nodesi, node_color='red', node_size = 2) 
-    #nx.draw_networkx_edges(h,pos=nodesi, edge_color='red', width = 2)
-    #fig = plt.gcf()
-    #fig.set_size_inches(8, 12)
-    #plt.show()
-    #filename = "ShortestWay__" + str(start) + "__" + str(end) + ".png"
-    #plt.savefig(filename, dpi = 100)
-    #fig.clear()
-    
 import math
 
 def ManhattanDist(start, finish):
@@ -301,7 +256,6 @@ def ChebDist(start, finish):
 
 def EuklidDist(start, finish):
     return math.sqrt((nodes.get(float(start))[0]-nodes.get(float(finish))[0])**2 + (nodes.get(float(start))[1]-nodes.get(float(finish))[1])**2)
-    
 
 import time
 
@@ -333,30 +287,6 @@ def timeAstarEukl(start, target):
     #print("--- %s seconds ---" % (time.time() - start_time))
     #drawShortestWay(GG, Astar_Eukl)
     
-
-def WriteCsvFile(start, target, way):
-    filename =  "ShortestWay__" + str(start) + "__" + str(target) + ".csv"
-    with open(filename, "a") as file:
-        writer = csv.writer(file)
-        writer.writerow(way)
-
-for t in target:
-    Dejicstra = nx.dijkstra_path(GG, str(start), str(t))
-    #DrawSaveShortestWay(GG, Dejicstra, str(start), str(t))
-    #WriteCsvFile(str(start), str(t), Dejicstra)
-    
-random = nodes.items()[1:1501:15]
-
-#fig = plt.gcf()
-#fig.set_size_inches(8, 12)   
-Lon_Lat = []
-
-#for r in random[1:30]:
-    #plt.plot(nodes.get(r[0])[1], nodes.get(r[0])[0], 'ro')
-#plt.plot(nodes.get(start)[1], nodes.get(start)[0],  'bo')
-
-#plt.show()
-
 filename =  "Statistics__100__points" + ".csv"
 
 fieldnames = ['idNode', 'Dejicstra', 'Levit', 'AstarEuklid','AstarManh', 'AstarCheb']
@@ -384,10 +314,164 @@ for nd in random:
     way = Levita(start, cur_node)
     levit = time.time() - start_time
     
-  
     line = [cur_node, dejicstr, levit, aStEukl, aStManh, aStCheb]
     inner_dict = dict(zip(fieldnames, line))
     line_dict.append(inner_dict)
-    #csv_writer(filename, fieldnames, line_dict)
+    csv_writer(filename, fieldnames, line_dict)
     
     
+filename =  "Statistics__100__points" + ".csv"
+
+node100 = []
+with open(filename, "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        node100.append(row[0])
+        
+    
+filename =  "TimeWay__100__points" + ".csv"
+
+fieldnames = ['idNode', 'time']
+
+with open(filename, "w") as file:
+    writer = csv.DictWriter(file, delimiter=',', fieldnames=fieldnames)
+    writer.writeheader()
+    
+def csv_writer(path, fieldnames, data):
+    with open(path, "a") as out_file:
+        writer = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
+        for row in data:
+            writer.writerow(row)
+
+for nd in node100[1:]:
+    way = Levita(start, int(nd));
+    len_way_km = 0
+    for i in range(len(way)-1):
+        nd_1 = nodes.get(way[i])
+        nd_2 = nodes.get(way[i + 1])
+        len_way_km = len_way_km +  math.sqrt(((nd_1[0] - nd_2[0])**2)*76057 + ((nd_1[1] - nd_2[1])**2)*111000)
+    time = len_way_km/40
+    line = []
+    line_dict = []
+    line = [nd, time]
+    inner_dict = dict(zip(fieldnames, line))
+    line_dict.append(inner_dict)
+    csv_writer(filename, fieldnames, line_dict)
+    
+    
+hamilton = [start]   
+way_between = {nd:[] for nd in target}
+
+way_to = []     
+to = [nd for nd in target]
+
+cur_way = 0
+len_cur_way = 0
+
+for useless in range(len(hospitals)):
+    
+    for nd in to:
+        cur_way = nx.dijkstra_path(GG, str(start), str(nd))
+        len_cur_way = 0
+        for i in range(len(cur_way)-1):
+            nd_1 = nodes.get(int(cur_way[i]))
+            nd_2 = nodes.get(int(cur_way[i + 1]))
+            len_cur_way = len_cur_way +  math.sqrt((nd_1[0] - nd_2[0])**2 + (nd_1[1] - nd_2[1])**2)
+
+        heappush(way_to, (len_cur_way, nd, cur_way))   
+    
+    next_node = heappop(way_to)[1]        
+    to.remove(next_node)                  
+    hamilton.append(next_node)            
+    start = next_node                    
+    way_to = []
+    
+    
+print hamilton
+
+node_to = [nd for nd in target]
+node_to.append(start)
+hamilton = list(tuple(node_to))
+size = len(node_to)  
+
+def lenght_edge(i, j):
+    l = math.sqrt((nodes.get(int(i))[0] - nodes.get(int(j))[0])**2 + (nodes.get(int(i))[1] - nodes.get(int(j))[1])**2)
+    return l
+
+def weight_way(way):
+    w = 0
+    for i in range(len(way) - 1):
+        w = w + lenght_edge(way[i], way[i+1])
+    return w
+
+edges = {}
+for i in node_to:
+    for j in node_to:
+        t = tuple([i,j])
+        way = nx.dijkstra_path(GG, str(i), str(j))   
+        edges[t] = weight_way(way)  
+
+min_weight = 1000000;
+min_edge = () 
+
+for key in edges:
+    if edges.get(key) < min_weight and edges.get(key)!=0 :
+        min_weight = edges.get(key)
+        min_edge = key
+        
+if min_edge[0] > min_edge[1]:
+    min_edge = ((min_edge[1],min_edge[0]))
+    
+node_to.remove(min_edge[0])    
+node_to.remove(min_edge[1])    
+
+
+cicle_edge = []    
+cicle_node = []    
+cicle_edge.append(min_edge)  
+cicle_node.append(min_edge[0])
+cicle_node.append(min_edge[1])
+
+    
+def lenght_way(cicle_ed, nd, ed):
+    w = 0
+    w = w + edges.get((ed[0], nd)) 
+    w = w + edges.get((ed[1], nd)) 
+    for nod in cicle_ed:
+        w = w + edges.get(nod)     
+    w = w - edges.get(ed)          
+    return w;
+    
+for useless in range(size - 2):
+    min_w_cicle = 1000000
+    ed_to_remove = ()
+    nd_to_add = 0
+    
+    for nd in node_to:
+        for ed in cicle_edge: 
+            w = lenght_way(tuple(cicle_edge), nd, ed)
+            if w < min_w_cicle:
+                min_w_cicle = w
+                ed_to_remove = ed
+                nd_to_add = nd
+    
+    
+    if ed_to_remove[0] < nd_to_add:
+        cicle_edge.append((ed_to_remove[0], nd_to_add))
+    else:
+        cicle_edge.append((nd_to_add, ed_to_remove[0]))
+        
+    if ed_to_remove[1] < nd_to_add:
+        cicle_edge.append((ed_to_remove[1], nd_to_add))
+    else:
+        cicle_edge.append((nd_to_add, ed_to_remove[1]))
+       
+    
+    if useless != 0:
+        cicle_edge.remove(ed_to_remove)
+    
+    cicle_node.append(nd_to_add)
+    
+    node_to.remove(nd_to_add)
+
+print cicle_edge
